b7dc72c71e623b884476f035a33317cfa63169e3
Dan Krishnan <dan@wearecodenation.com>
2019-11-20 17:20:08 +0000

    Updates

---
+++ precache-manifest.d506228861252343f2e0455b48805d5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3caf87476ed1959bc990b0518a23ed",
+    "url": "/infuriatingmemorygame/index.html"
+  },
+  {
+    "revision": "24d972dd3851140f82bf",
+    "url": "/infuriatingmemorygame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1eb6bdf559ddaa85fa08",
+    "url": "/infuriatingmemorygame/static/js/2.091621e9.chunk.js"
+  },
+  {
+    "revision": "24d972dd3851140f82bf",
+    "url": "/infuriatingmemorygame/static/js/main.27af8fea.chunk.js"
+  },
+  {
+    "revision": "3264a29515085a9b00a0",
+    "url": "/infuriatingmemorygame/static/js/runtime-main.7bd9a20e.js"
+  },
+  {
+    "revision": "69f44bd2e7c7279b1925d677e673c54f",
+    "url": "/infuriatingmemorygame/static/media/bowser.69f44bd2.jpg"
+  },
+  {
+    "revision": "a782540d55f95dfc85a315c587f5d38d",
+    "url": "/infuriatingmemorygame/static/media/luigi.a782540d.png"
+  },
+  {
+    "revision": "b287c594ce90007b6ab19f2f1fe25b8e",
+    "url": "/infuriatingmemorygame/static/media/mario-pose4.b287c594.png"
+  },
+  {
+    "revision": "9879d90b731293a392f2fc477bc58b46",
+    "url": "/infuriatingmemorygame/static/media/tum.9879d90b.jpg"
+  },
+  {
+    "revision": "e5b3de9cd4e0cd5a51267cc6f25c81d8",
+    "url": "/infuriatingmemorygame/static/media/ugandanKnuckles.e5b3de9c.jpeg"
+  }
+]);
